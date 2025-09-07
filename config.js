3d8879b276b9e386bd3a0abfecaec1920d17fcc5
Mrhacker021 <mrbesturdu65@gmail.com>
2025-09-07 22:18:58 +0500

    Update config.js

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923237045910",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923092762597",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "*𝕶𝖚𝖒𝖆𝖎𝖑_𝑀𝒟 Official*",
 // add bot owner name
