41e630865df10a9cdcdeede7e58ab5eef718be0d
Mrhacker021 <mrbesturdu65@gmail.com>
2025-09-07 22:11:49 +0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -44,7 +44,7 @@ async (conn, mek, m, { from }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363348739987203@newsletter',
+                    newsletterJid: '120363399579001828@g.us',
                     newsletterName: '𝕶𝖚𝖒𝖆𝖎𝖑_𝑀𝒟',
                     serverMessageId: 143
                 }            
